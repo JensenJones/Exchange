2b25a7f8ace1aa3010605ef6584e245e8090922f
jensen <jensen.jones111@gmail.com>
2025-01-24 11:40:00 +1100

    Updated ServerMain Changed to dynamically allocates a free port

--- Server/src/main/java/org/jj/orderService/OrderServiceServer.java
+++ Server/src/main/java/org/jj/orderService/OrderServiceServer.java
@@ -9,6 +9,7 @@ import org.jj.product.ProductStore;
 import org.jj.providers.IntIdProvider;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 
 public class OrderServiceServer {
 
@@ -16,7 +17,7 @@ public class OrderServiceServer {
     private final Server server;
 
     public OrderServiceServer(int port) {
-        this.port = port;
+        this.port = (port == 0) ? findFreePort() : port;
         this.server = ServerBuilder.forPort(this.port)
                 .addService(new OrderServiceImpl(new MatchingEngineProvider(new ProductStore(new IntIdProvider()))
                 , new OrderStore()))
@@ -50,6 +51,14 @@ public class OrderServiceServer {
     public void blockUntilShutdown() throws InterruptedException {
         if (server != null) {
             server.awaitTermination();
+        }
+    }
+
+    private int findFreePort() {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            return socket.getLocalPort();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to find a free port", e);
         }
     }
 }
--- Server/src/main/java/org/jj/orderService/ServerMain.java
+++ Server/src/main/java/org/jj/orderService/ServerMain.java
@@ -1,11 +1,9 @@
 package org.jj.orderService;
 
-import java.net.ServerSocket;
-
 public class ServerMain {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(0)) {
-            OrderServiceServer server = new OrderServiceServer(serverSocket.getLocalPort());
+        try {
+            OrderServiceServer server = new OrderServiceServer(0);
             server.start();
             server.blockUntilShutdown();
         } catch (Exception e) {
